941e56ea0f9cf4e6d3084b5d13d6c4f3c3e8b886
moussaab moulim <42440629+moussaab-moulim@users.noreply.github.com>
2022-10-20 00:14:14 +0200

    hot fix next config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withBundleAnalyzer({
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     images: {
-        format: ["image/webp", "image/avif"],
+        formats: ["image/webp", "image/avif"],
         domains: ["olistik-laser.cdn.prismic.io", "images.prismic.io"],
         deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
