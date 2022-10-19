7fb95e30bed8fd31c2336b103acf030889ec3d5d
moussaab moulim <42440629+moussaab-moulim@users.noreply.github.com>
2022-10-19 03:47:06 +0200

    adding google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,12 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { GTM_ID } from "@services/gtm";
+import Document, {
+    DocumentContext,
+    DocumentInitialProps,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from "next/document";
 
 class CustomDocument extends Document {
     static async getInitialProps(
@@ -7,6 +15,26 @@ class CustomDocument extends Document {
         const initialProps = await Document.getInitialProps(ctx);
 
         return initialProps;
+    }
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <noscript>
+                        <iframe
+                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                            height="0"
+                            width="0"
+                            style={{ display: "none", visibility: "hidden" }}
+                        ></iframe>
+                    </noscript>
+
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
 
